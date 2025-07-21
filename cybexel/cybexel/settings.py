8cfa704f3f0ca80980d2dd23c596c334a4a5b633
N-meapp <developersnme@gmail.com>
2025-07-21 12:56:53 +0530

    updated

--- cybexel/cybexel/settings.py
+++ cybexel/cybexel/settings.py
@@ -73,14 +73,20 @@ WSGI_APPLICATION = 'cybexel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'cybexeldb',
+#         'USER': 'myuser',
+#         'PASSWORD': 'clyebxe',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cybexeldb',
-        'USER': 'myuser',
-        'PASSWORD': 'clyebxe',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
