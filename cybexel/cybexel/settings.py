f9b449dc68fe535fa381c424dfe28f48445a3e13
N-meapp <developersnme@gmail.com>
2025-10-28 13:07:34 +0530

    updated

--- cybexel/cybexel/settings.py
+++ cybexel/cybexel/settings.py
@@ -122,12 +122,12 @@ CSRF_TRUSTED_ORIGINS = [
     "https://www.cybexel.com",
 ]
   
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
