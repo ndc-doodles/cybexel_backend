24f2336b8b6bb7a55c724474b82f1d46817c5dca
zayanft <anshidp2e@gmail.com>
2026-01-30 17:51:33 +0530

    political

--- cybexel/cybexel/settings.py
+++ cybexel/cybexel/settings.py
@@ -26,11 +26,11 @@ ALLOWED_HOSTS = [
 
 
 
-# ALLOWED_HOSTS = []
-# CSRF_TRUSTED_ORIGINS = [
-#     "https://cybexel.com",
-#     "https://www.cybexel.com",
-# ]
+ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = [
+    "https://cybexel.com",
+    "https://www.cybexel.com",
+]
 
 # Applications
 INSTALLED_APPS = [
@@ -117,10 +117,10 @@ DATABASES = {
 }
 
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://cybexel.com",
-    "https://www.cybexel.com",
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     "https://cybexel.com",
+#     "https://www.cybexel.com",
+# ]
   
 # DATABASES = {
 #     'default': {
