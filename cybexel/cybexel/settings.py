889b20586f26ab54b1ba0af4491bb40640ceb108
N-meapp <developersnme@gmail.com>
2026-02-21 14:06:15 +0530

    updated

--- cybexel/cybexel/settings.py
+++ cybexel/cybexel/settings.py
@@ -6,99 +6,76 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-# ---------------- BASE ----------------
+# Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Load environment variables from .env
+# Load environment variables
 load_dotenv(BASE_DIR / ".env")
 
-# ---------------- SECURITY ----------------
-SECRET_KEY = os.getenv(
-    "SECRET_KEY",
-    "django-insecure-emohm$#-n%0%ade0@a6xe_tj&+yc+gxgqh*d$p)wt$vormu%t3"
-)
-DEBUG = os.getenv("DEBUG", "True") == "True"
+# SECURITY
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-emohm$#-n%0%ade0@a6xe_tj&+yc+gxgqh*d$p)wt$vormu%t3')
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    "0.0.0.0",
-    "127.0.0.1",
-    "localhost",
-    "cybexel.com",
-    "www.cybexel.com",
+    '0.0.0.0',
+    '127.0.0.1',
+    'localhost',
+    'cybexel.com',
+    'www.cybexel.com',
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://cybexel.com",
-    "https://www.cybexel.com",
-]
 
-# ---------------- APPLICATIONS ----------------
+
+# ALLOWED_HOSTS = []
+# CSRF_TRUSTED_ORIGINS = [
+#     "https://cybexel.com",
+#     "https://www.cybexel.com",
+# ]
+
+# Applications
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "cybexelapp",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'cybexelapp',
 ]
 
-# ---------------- MIDDLEWARE ----------------
+# Middleware
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# ---------------- URLS & WSGI ----------------
-ROOT_URLCONF = "cybexel.urls"
-WSGI_APPLICATION = "cybexel.wsgi.application"
+ROOT_URLCONF = 'cybexel.urls'
 
-# ---------------- TEMPLATES ----------------
+# Templates
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-# ---------------- DATABASE ----------------
-# PostgreSQL configuration (from .env)
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+WSGI_APPLICATION = 'cybexel.wsgi.application'
 
-if not all([DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT]):
-    raise Exception(
-        "Database environment variables are not set properly in .env!"
-    )
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": DB_NAME,
-        "USER": DB_USER,
-        "PASSWORD": DB_PASSWORD,
-        "HOST": DB_HOST,
-        "PORT": DB_PORT,
-    }
-}
-
-# ---------------- LOCAL/SQLite (commented out) ----------------
+# Database (using SQLite by default)
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
@@ -106,33 +83,79 @@ DATABASES = {
 #     }
 # }
 
-# ---------------- AUTH PASSWORD ----------------
+# If you want PostgreSQL, uncomment and configure:
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST", "localhost"),
+#         "PORT": os.getenv("DB_PORT", "5432"),
+#     }
+# }
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+
+import os
+  # Load .env file
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://cybexel.com",
+    "https://www.cybexel.com",
+]
+  
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# Password validation
 AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-# ---------------- INTERNATIONALIZATION ----------------
-LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# ---------------- STATIC & MEDIA ----------------
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# Static files
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # for collectstatic in production
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
-# ---------------- DEFAULT PK ----------------
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# ---------------- LOGIN ----------------
-LOGIN_URL = "admin_login"
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login
+LOGIN_URL = 'admin_login'
 
 # Email (example: Hostinger)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
