c6336b501ba9eddfb80b076c13b9521f264acd1f
N-meapp <developersnme@gmail.com>
2026-02-21 12:08:35 +0530

    updated

--- cybexel/cybexel/settings.py
+++ cybexel/cybexel/settings.py
@@ -27,10 +27,10 @@ ALLOWED_HOSTS = [
 
 
 # ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = [
-    "https://cybexel.com",
-    "https://www.cybexel.com",
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     "https://cybexel.com",
+#     "https://www.cybexel.com",
+# ]
 
 # Applications
 INSTALLED_APPS = [
@@ -146,15 +146,11 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # for collectstatic in production
 
-# Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
-
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
